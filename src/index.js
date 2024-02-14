efc9ad3e30ab3539437cbf66b4dd09edb1b40c0d
souravpanigrahilq <sourav.panigrahi@lumiq.ai>
2024-02-14 11:24:53 +0530

    Done with the edits

--- src/index.js
+++ src/index.js
@@ -8,15 +8,14 @@ const router = require("./routes");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+router.get("/", (req, res) => {
+  res.send("Welcome to my server from the router!");
+});
+
 app.use("/", router);
 
 app.listen(PORT, async () => {
   console.log(`Server started running successfully on PORT:${PORT}`);
   connect();
   console.log("Mongo DB connected");
-  User.create({
-    name: "Sourav",
-    mail: "pandu.com",
-    age: 16,
-  });
 });
