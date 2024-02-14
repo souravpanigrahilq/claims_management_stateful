4cbce6f760c2ab90cb15272b9aceadb28d4515c9
souravpanigrahilq <sourav.panigrahi@lumiq.ai>
2024-02-14 13:00:01 +0530

    your message

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,16 @@
+require("dotenv").config();
+
 const mongoose = require("mongoose");
+
 const connect = async () => {
-  await mongoose.connect(
-    "mongodb+srv://souravpanigrahi:sourav123@cluster0.chmi1or.mongodb.net/?retryWrites=true&w=majority"
-  );
+  try {
+    console.log("MongoDB URI:", process.env.MONGO_URI); // Debugging statement
+
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to MongoDB!");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
 };
 
 module.exports = connect;
